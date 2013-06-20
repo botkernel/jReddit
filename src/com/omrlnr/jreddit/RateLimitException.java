574c4b0f4f59ddfd814d6812ad1d9fae0a645728
jasonsimpson <jason.c.simpson@gmail.com>
2013-06-20 14:16:56 -0700

    Add capability to retrieve retry time from rate limit exception.

--- src/com/omrlnr/jreddit/RateLimitException.java
+++ src/com/omrlnr/jreddit/RateLimitException.java
@@ -17,4 +17,42 @@ public class RateLimitException extends IOException {
     public RateLimitException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    /**
+     *
+     * Returns time in seconds the rate limit error is asking
+     *          for us to wait.
+     *
+     * @return time in seconds
+     *
+     */
+    public int getRetryTime() {
+        int ret = -1;
+
+        //
+        // Example messages to parse:
+        // "you are doing that too much. try again in 9 minutes."
+        //
+        // "you are doing that too much. try again in 34 seconds."
+        //
+        String message = getMessage();
+
+        String time = message.substring(
+                            message.indexOf("try again in ") + 
+                            "try again in ".length() );
+        time = time.substring(0, time.indexOf(" "));
+
+        try {
+            ret = Integer.parseInt(time);
+            if(message.indexOf("minutes") != -1) {
+                // We need to wait minutes before retrying.
+                ret = ret * 60;
+            }
+        } catch(NumberFormatException nfe) {
+
+        }
+
+        return ret;
+    }
+
 }
